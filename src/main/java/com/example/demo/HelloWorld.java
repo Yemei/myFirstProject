35f45caad0bc17d5e281c7962380422e2298678a
yemeiz <1312477469@qq.com>
2017-09-18 11:38:01 +0800

    spring boot test

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
